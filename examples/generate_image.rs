3385a1b9bdc49b1b0c2593e3e6fe6bb512d88d05
Lutica CANARD <presan100@gmail.com>
2025-08-31 01:48:01 +0900

    format change

--- examples/generate_image.rs
+++ examples/generate_image.rs
@@ -1,8 +1,8 @@
-use std::fs;
 #[cfg(feature = "use-base64")]
 use base64::Engine;
 #[cfg(feature = "use-base64")]
-use base64::{engine::general_purpose};
+use base64::engine::general_purpose;
+use std::fs;
 // function calling example
 use gemini_rs::types::{InlineData, Part};
 use serde::{Deserialize, Serialize};
@@ -19,7 +19,7 @@ pub enum InlineDataMimeTypeEnum {
     #[serde(rename = "image/heic")]
     ImageHeic,
     #[serde(rename = "image/heif")]
-    ImageHeif
+    ImageHeif,
 }
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -27,33 +27,46 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut chat_list = vec![];
     chat_list.push(Part::text("Remake Tree well"));
-    #[cfg(feature = "use-base64")]{
+    #[cfg(feature = "use-base64")]
+    {
         chat_list.push(Part::inline_data(
-        "image/jpeg",
-        &general_purpose::STANDARD.encode(
-                &fs::read("examples/images/tree.jpg")?
-            ),
+            "image/jpeg",
+            &general_purpose::STANDARD.encode(&fs::read("examples/images/tree.jpg")?),
         ));
     }
 
     let response = client
-    .chat("gemini-2.5-flash-image-preview")
-    .send_parted_messages(chat_list).await?;
-    let image = response.candidates[0].content.parts.iter().filter(|a| {
-        if let Part { inline_data: Some(InlineData { mime_type:_, data:_ }), .. } = a {
-            true
-        } else {
-            false
-        }
-    }).map(|img| {
-        img.inline_data.as_ref().unwrap().data.clone()
-    });
+        .chat("gemini-2.5-flash-image-preview")
+        .send_parted_messages(chat_list)
+        .await?;
+    let image = response.candidates[0]
+        .content
+        .parts
+        .iter()
+        .filter(|a| {
+            if let Part {
+                inline_data:
+                    Some(InlineData {
+                        mime_type: _,
+                        data: _,
+                    }),
+                ..
+            } = a
+            {
+                true
+            } else {
+                false
+            }
+        })
+        .map(|img| img.inline_data.as_ref().unwrap().data.clone());
 
-    #[cfg(feature = "use-base64")]{
+    #[cfg(feature = "use-base64")]
+    {
         let image_data = general_purpose::STANDARD.decode(image.collect::<Vec<_>>()[0].as_str())?;
         let _ = fs::write("examples/images/response.png", &image_data)?;
     }
-    #[cfg(not(feature = "use-base64"))]{
+    #[cfg(not(feature = "use-base64"))]
+    {
         let collected = image.collect::<Vec<_>>();
         let image_data = collected[0].as_str();
         let _ = fs::write("examples/images/response.json", image_data)?;
